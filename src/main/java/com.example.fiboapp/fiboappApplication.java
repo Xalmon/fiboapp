f8d95871371732663b0fa1a6bb6c15b13b5cea1f
Xalmon <xalmonoyefule@gmail.com>
2024-08-16 10:45:22 +0100

    create a springboot project from bash, push to git and auto create branches and push to dockerhub

---
+++ src/main/java/com.example.fiboapp/fiboappApplication.java
@@ -0,0 +1,12 @@
+package com.example.fiboapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class fiboappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(fiboappApplication.class, args);
+  }
+}
